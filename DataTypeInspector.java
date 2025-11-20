803e32b79fd89619dbbcce23a8fc922cbae6a26d
Aashra Singh <aashrasingh282@gmail.com>
2025-11-20 11:51:48 +0545

    data type inspector que 3

---
+++ DataTypeInspector.java
@@ -0,0 +1,27 @@
+
+/**
+ * Write a description of class DataTypeInspector here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class DataTypeInspector
+{
+    public static void main(String[] args){
+        int a=10;
+        float b=4;
+        byte c=1;
+        short d=20;
+        double e=2.909;
+        char f='a';
+        boolean g=true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+    
+    }
+}
